1948f48444bdcb7b894606cd54ede8b956d584fa
Remy Goldschmidt <taktoa@gmail.com>
2018-04-16 14:31:13 -0500

    Start work on testing infrastructure

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -7,15 +7,28 @@ module Main
 
 --------------------------------------------------------------------------------
 
-import           Tests.Integration (integrationTests)
-import           Tests.Unit        (unitTests)
+import           Tests.Integration                      (integrationTests)
+import           Tests.Unit                             (unitTests)
+
+import qualified Test.Tasty                             as Tasty
+import qualified Test.Tasty.Ingredients                 as Tasty
+import qualified Test.Tasty.Ingredients.Basic           as Tasty
+import qualified Test.Tasty.Ingredients.ConsoleReporter as Tasty
+import qualified Test.Tasty.Options                     as Tasty
+import qualified Test.Tasty.Providers                   as Tasty
+import qualified Test.Tasty.Runners                     as Tasty
 
 --------------------------------------------------------------------------------
 
 -- | FIXME: doc
 main :: IO ()
 main = do
-  unitTests
-  integrationTests
+  us <- unitTests
+  is <- integrationTests
+  let tests = Tasty.testGroup "Tests"
+              [ Tasty.testGroup "Unit tests" us
+              , Tasty.testGroup "Integration tests" is
+              ]
+  Tasty.defaultMain tests
 
 --------------------------------------------------------------------------------
--- tests/Tests/Integration.hs
+++ tests/Tests/Integration.hs
@@ -7,13 +7,49 @@ module Tests.Integration
 
 --------------------------------------------------------------------------------
 
-import           Flow ((.>), (|>))
+import qualified EqSat                                  as EqSat
+import           EqSat.Domain                           (Domain)
+import           EqSat.Equation                         (Equation)
+import qualified EqSat.Equation                         as Equation
+import qualified EqSat.Internal.GraphMatching           as GraphMatching
+import           EqSat.Internal.Matrix                  (Matrix)
+import qualified EqSat.Internal.Matrix                  as Matrix
+import qualified EqSat.Internal.MGraph                  ()
+import qualified EqSat.Internal.MGraph                  as MGraph
+import           EqSat.Internal.MHashMap                (MHashMap)
+import qualified EqSat.Internal.MHashMap                as MHashMap
+import           EqSat.Internal.MHashSet                (MHashSet)
+import qualified EqSat.Internal.MHashSet                as MHashSet
+import           EqSat.Internal.MutableBitmap           (MutableBitmap)
+import qualified EqSat.Internal.MutableBitmap           as MutableBitmap
+import           EqSat.IsExpression
+                 (IsExpression (exprToTerm, termToExpr))
+import           EqSat.Term                             (Term)
+import qualified EqSat.Term                             as Term
+import           EqSat.Variable                         (Variable)
+import qualified EqSat.Variable                         as Variable
+
+import qualified Hedgehog                               as HH
+import qualified Hedgehog.Gen                           as HH.Gen
+import qualified Hedgehog.Range                         as HH.Range
+
+import           Test.Tasty                             (TestTree)
+import qualified Test.Tasty                             as Tasty
+import qualified Test.Tasty.Hedgehog                    as Tasty.HH
+import qualified Test.Tasty.Ingredients                 as Tasty
+import qualified Test.Tasty.Ingredients.Basic           as Tasty
+import qualified Test.Tasty.Ingredients.ConsoleReporter as Tasty
+import qualified Test.Tasty.Options                     as Tasty
+import qualified Test.Tasty.Providers                   as Tasty
+import qualified Test.Tasty.Runners                     as Tasty
+
+import           Flow                                   ((.>), (|>))
 
 --------------------------------------------------------------------------------
 
 -- | FIXME: doc
-integrationTests :: IO ()
+integrationTests :: IO [TestTree]
 integrationTests = do
-  pure ()
+  pure []
 
 --------------------------------------------------------------------------------
