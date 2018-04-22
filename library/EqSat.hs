a6c0bfb6099698327dcbbd1bd5bc48ae18c4618a
Remy Goldschmidt <taktoa@gmail.com>
2018-04-22 13:49:23 -0500

    Split TypeSystem out into a separate module

--- library/EqSat.hs
+++ library/EqSat.hs
@@ -125,72 +125,13 @@ import           EqSat.Domain                 (Domain)
 import           EqSat.IsExpression
                  (IsExpression (exprToGTerm, gtermToExpr))
 
+import           EqSat.TypeSystem
+                 (TypeSystem (Type, TypeError, inferType, isSubtype))
+import qualified EqSat.TypeSystem             as TypeSystem
+
 import qualified EqSat.Internal.PrettyPrinter as PP
 
 --------------------------------------------------------------------------------
-
--- | FIXME: doc
-class (IsExpression node expr) => TypeSystem node expr where
-  -- | A type whose values represent the types of expressions.
-  data Type expr
-
-  -- | A type whose values represent type errors.
-  data TypeError expr
-
-  -- | Infer the type of an open term.
-  --
-  --   Takes as input a proxy value (e.g.: 'Proxy') to avoid use of
-  --   @-XAllowAmbiguousTypes@ as well as an open 'Term' with variables
-  --   in an arbitrary @var@ type.
-  --
-  --   Returns a @'Maybe' ('TypedTerm' repr var node expr)@.
-  --   'Nothing' should only be returned in case of a failure in type inference.
-  --
-  --   The 'Ord' and 'Hashable' instances are there so that you can efficiently
-  --   create a 'TypedTerm' with a 'Map' or a 'HashMap' for the types of the
-  --   variables.
-  inferType
-    :: (Ord var, Hashable var, MonadError (TypeError expr) m)
-    => Term repr node var
-    -> m (TypedTerm repr node var (Type expr))
-
-  -- | Return 'True' if the first given 'Type' is a subtype of the second
-  --   given 'Type' in your type system, and return 'False' otherwise.
-  --
-  --   This function takes as input a proxy value (e.g.: 'Proxy') to avoid use
-  --   of @-XAllowAmbiguousTypes@.
-  --
-  --   Laws:
-  --
-  --   1. This should be a total function.
-  --   2. This should be a preorder (transitive and reflexive).
-  isSubtype
-    :: Type expr
-    -> Type expr
-    -> Bool
-
-  -- FIXME: maybe `isSubtype` should have type
-  --        `(MonadError (TypeError expr) m) => Type expr -> Type expr -> m ()`.
-
-  -- | Pretty-print a @'TypeError' expr@ as a 'PP.Doc'.
-  --   This version does not allow any annotations.
-  showTypeError
-    :: TypeError expr
-    -> PP.Doc ann
-
-  -- | Pretty-print a @'TypeError' expr@ as a 'PP.Doc'.
-  --   This version allows you to add ANSI color annotations, if you want.
-  --   If you don't want to add ANSI coloring, there is a default definition
-  --   in terms of 'showTypeError'.
-  --
-  --   Laws:
-  --
-  --   1. For any @e ∈ 'TypeError' expr@ and @p ∈ 'Proxy' expr@,
-  --      @(\\_ → ()) '<$>' 'showTypeErrorANSI' p e ≡ 'showTypeError' p e@.
-  showTypeErrorANSI
-    :: TypeError expr
-    -> PP.Doc PP.AnsiStyle
-  showTypeErrorANSI = showTypeError
 
 -- | A datatype representing one side of an equation.
 data EquationSide
