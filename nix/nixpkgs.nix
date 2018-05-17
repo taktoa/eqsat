6824ec159db957d91d76e1106916897608bead51
Remy Goldschmidt <taktoa@gmail.com>
2018-05-17 02:05:09 -0500

    Nix build updated to GHC 8.2.2

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -4,8 +4,8 @@ with rec {
   nixpkgs = fetchFromGitHub {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    rev    = "12512f25e9ecdf6e8fc97a80297a4d2fa89e2f99";
-    sha256 = "1g0wn93szifqyvd6n131j12m9vjw6lbpy2hms18sv1imw2lxsza1";
+    rev    = "06c576b0525da85f2de86b3c13bb796d6a0c20f6";
+    sha256 = "01cra89drfjf3yhii5na0j5ivap2wcs0h8i0xcxrjs946nk4pp5j";
   };
 };
 
@@ -15,18 +15,6 @@ import nixpkgs {
       haskellPackages = super.haskellPackages.override {
         overrides = import ./overrides.nix { pkgs = self; };
       };
-
-      z3 = (super.z3.override { python2 = super.python; }).overrideAttrs (old: rec {
-        name = "z3-${version}";
-        version = "4.6.0";
-        src = super.fetchFromGitHub {
-          owner  = "Z3Prover";
-          repo   = "z3";
-          rev    = "b0aaa4c6d7a739eb5e8e56a73e0486df46483222";
-          sha256 = "1cgwlmjdbf4rsv2rriqi2sdpz9qxihxrcpm6a4s37ijy437xg78l";
-        };
-        propagatedBuildInputs = [ super.python.pkgs.setuptools ];
-      });
     };
   };
   overlays = [];
